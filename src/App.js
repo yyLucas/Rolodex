9a067b15cd937e10c0140203d638ac5c7f1c1680
Lucas Zhang <zhangysd@hotmail.com>
2022-07-03 21:51:28 +1200

    Add search box

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            monsters: [],
+            searchString: '',
+        }
+    }
+
+    componentDidMount() {
+        fetch("https://jsonplaceholder.typicode.com/users")
+            .then(response => response.json())
+            .then(data => this.setState({ monsters: data }));
+    }
+
+    render() {
+        const filteredMonsters = this.state.monsters.filter(monster => {
+            return monster.name.toLowerCase().includes(this.state.searchString);
+        });
+
+        return (
+            <div className="App">
+                <input
+                    className='search-box'
+                    type='search'
+                    placeholder='search monsters'
+                    onChange={(event) => {
+                        const searchString = event.target.value.toLowerCase();
+                        this.setState({searchString})
+                    }}
+                />
+                {filteredMonsters.map(monster => {
+                    return (
+                        <div key={monster.id}>
+                            <h1>{monster.name}</h1>
+                        </div>
+                    );
+                })}
+            </div>
+        );
+    };
 }
 
 export default App;
